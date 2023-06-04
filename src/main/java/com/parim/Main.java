6585d58c38a84203441e913266f1257ba0365202
Parisa <mousaviparisa5@yahoo.com>
2023-06-04 12:18:04 +0330

    Connect main.java to GameController

--- src/main/java/com/parim/Main.java
+++ src/main/java/com/parim/Main.java
@@ -1,6 +1,9 @@
 package com.parim;
 
+import com.parim.controller.GameController;
+
 public class Main {
     public static void main(String[] args) {
+        GameController.getInstance().run();
     }
 }
