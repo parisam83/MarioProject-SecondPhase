9155fe1f17717abfdd8970d8e126cf2c5bbe59ef
Parisa <mousaviparisa5@yahoo.com>
2023-06-27 19:10:53 +0330

    Add interfaces Movable and Convertible

---
+++ src/main/java/com/parim/model/interfaces/Convertible.java
@@ -0,0 +1,9 @@
+package com.parim.model.interfaces;
+
+import com.parim.model.components.TileObject;
+
+import java.util.ArrayList;
+
+public interface Convertible {
+    ArrayList<TileObject> convert();
+}
