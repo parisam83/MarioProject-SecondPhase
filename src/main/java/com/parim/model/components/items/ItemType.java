b75cb0c5d764cd434033498c760ea2fadf8836cf
Parisa <mousaviparisa5@yahoo.com>
2023-06-27 19:15:04 +0330

    Add enum ItemType with random item generator method

---
+++ src/main/java/com/parim/model/components/items/ItemType.java
@@ -0,0 +1,23 @@
+package com.parim.model.components.items;
+
+import java.util.concurrent.ThreadLocalRandom;
+
+public enum ItemType {
+    COIN(2),
+    STAR(5),
+    MUSHROOM(3),
+    FLOWER(1);
+
+    private final int weight;
+    ItemType(int weight){
+        this.weight = weight;
+    }
+
+    public static ItemType randomItem(){
+        int randomNumber = ThreadLocalRandom.current().nextInt(1, (2 + 5 + 3 + 1) + 1);
+        if (randomNumber <= COIN.weight) return COIN;
+        if (randomNumber <= COIN.weight + STAR.weight) return STAR;
+        if (randomNumber <= COIN.weight + STAR.weight + MUSHROOM.weight) return MUSHROOM;
+        return FLOWER;
+    }
+}
