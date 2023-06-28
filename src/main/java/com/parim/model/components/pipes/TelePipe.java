6e4d5bec5e160131d0193af23e016d5c44985522
Parisa <mousaviparisa5@yahoo.com>
2023-06-28 20:09:17 +0330

    Change width and height of pipes

--- src/main/java/com/parim/model/components/pipes/TelePipe.java
+++ src/main/java/com/parim/model/components/pipes/TelePipe.java
@@ -6,13 +6,13 @@ public class TelePipe extends Pipe {
     protected SectionObject section;
 
     public TelePipe() {
-        height = 3;
+        // height = 3;
     }
 
     public TelePipe(double x, double y, SectionObject section) {
         super(x, y);
         this.section = section;
-        height = 3;
+        // height = 3;
     }
 
     // Getters and setters
