fb0be8f92102fc4c904eef6e1d2e74051db4a874
Parisa <mousaviparisa5@yahoo.com>
2023-06-28 12:16:02 +0330

    Change Movable interface

--- src/main/java/com/parim/model/components/MarioObject.java
+++ src/main/java/com/parim/model/components/MarioObject.java
@@ -3,9 +3,9 @@ package com.parim.model.components;
 import com.parim.model.interfaces.Movable;
 
 public class MarioObject extends TileObject implements Movable {
+    public static double SPEED_RIGHT = 0.1, SPEED_UP = 0.1;
     public MarioObject(double x, double y) {
         super(x, y);
-        speed = 0.1;
     }
 
     @Override
@@ -17,5 +17,25 @@ public class MarioObject extends TileObject implements Movable {
     public void move() {
         x += xVelocity;
         y += yVelocity;
+    }
+
+    @Override
+    public void updateVelocityMoveRight() {
+        xVelocity = SPEED_RIGHT;
+    }
+
+    @Override
+    public void updateVelocityMoveLeft() {
+        xVelocity = -SPEED_RIGHT;
+    }
+
+    @Override
+    public void updateVelocityMoveUp() {
+        yVelocity = -SPEED_UP;
+    }
+
+    @Override
+    public void updateVelocityMoveDown() {
+        yVelocity = SPEED_UP;
     }
 }
--- src/main/java/com/parim/model/interfaces/Movable.java
+++ src/main/java/com/parim/model/interfaces/Movable.java
@@ -2,4 +2,8 @@ package com.parim.model.interfaces;
 
 public interface Movable {
     void move();
+    void updateVelocityMoveRight();
+    void updateVelocityMoveLeft();
+    void updateVelocityMoveUp();
+    void updateVelocityMoveDown();
 }
