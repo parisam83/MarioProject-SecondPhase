2d673f220e552e4980226f3f41da6920ace04a6a
Parisa <mousaviparisa5@yahoo.com>
2023-06-29 10:26:05 +0330

    Modify GamePanel to show items and update it from GameController

--- src/main/java/com/parim/view/GamePanel.java
+++ src/main/java/com/parim/view/GamePanel.java
@@ -6,6 +6,7 @@ import com.parim.model.SectionObject;
 import com.parim.model.components.*;
 import com.parim.model.components.blocks.Block;
 import com.parim.model.components.enemies.Enemy;
+import com.parim.model.components.items.Item;
 import com.parim.model.components.pipes.Pipe;
 import com.parim.view.loaders.ImageLoader;
 
@@ -22,6 +23,7 @@ public class GamePanel extends JPanel {
     private ArrayList<Block> blocks = section.getBlocks();
     private ArrayList<Enemy> enemies = section.getEnemies();
     private ArrayList<Pipe> pipes = section.getPipes();
+    private ArrayList<Item> items = new ArrayList<>();
 
     public GamePanel(){
         this.setLayout(null);
@@ -41,12 +43,18 @@ public class GamePanel extends JPanel {
             g.drawImage(ImageLoader.getInstance().LoadObject("/enemies/" + enemy.getType(), enemy.getWidth(), enemy.getHeight()), (int) (enemy.getX() * TileObject.getSIZE()), (int) ((9.5 - enemy.getY()+2) * TileObject.getSIZE()), null);
         for (Pipe pipe : pipes)
             g.drawImage(ImageLoader.getInstance().LoadObject("/pipes/" + pipe.getType(), pipe.getWidth(), pipe.getHeight()), (int) ((pipe.getX()-pipe.getWidth()+1) * TileObject.getSIZE()), (int) ((9.5 - pipe.getY() - pipe.getHeight()+3) * TileObject.getSIZE()), null);
+        if (items.size() > 0)
+            for (Item item : items) {
+                System.out.println(items.size());
+                g.drawImage(ImageLoader.getInstance().LoadObject("/items/" + item.getType(), item.getWidth(), item.getHeight()), (int) ((item.getX()) * TileObject.getSIZE()), (int) ((9.5 - item.getY() - item.getHeight() + 3) * TileObject.getSIZE()), null);
+            }
     }
 
     private void updateTiles() {
         blocks = GameController.getInstance().getBlocks();
         pipes = GameController.getInstance().getPipes();
         enemies = GameController.getInstance().getEnemies();
+        items = GameController.getInstance().getItems();
     }
 
     public class KL extends KeyAdapter{
